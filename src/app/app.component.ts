ea436331e8267a50395068ca76f26bd45c025f69
harshit <h.d.chhajed@gmail.com>
2020-07-28 18:01:29 +0530

    Facebook login place correct, logout for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,11 @@ export class AppComponent implements OnInit {
         appId      : '635708387067929',
         cookie     : true,
         xfbml      : true,
-        version    : 'v7.0'
+        version    : 'v7.0',
       });
 
       FB.AppEvents.logPageView();
+
     };
 
     ((d, s, id) => {
@@ -31,36 +32,5 @@ export class AppComponent implements OnInit {
       fjs.parentNode.insertBefore(js, fjs);
     })(document, 'script', 'facebook-jssdk');
 
-  }
-
-
-  fbSubmitLogin(){
-    FB.getLoginStatus((response: any) => {   // Called after the JS SDK has been initialized.
-      this.statusChangeCallback(response);        // Returns the login status.
-    });
-
-    console.log('submit login to facebook');
-
-    FB.login((response: any) => {
-      console.log(response);
-      this.statusChangeCallback(response);
-    }, {scope: 'public_profile,email'});
-  }
-
-  statusChangeCallback(response: any) {  // Called with the results from FB.getLoginStatus().
-    console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-      this.testAPI();
-    } else {                                 // Not logged into your webpage or we are unable to tell.
-      console.log('Log in failed');
-    }
-  }
-
-  testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', (response: any) => {
-      console.log('Successful login for: ' + response.name);
-    });
   }
 }
--- src/app/header/dialog-popup/dialog-popup.component.ts
+++ src/app/header/dialog-popup/dialog-popup.component.ts
@@ -12,39 +12,46 @@ export class DialogPopupComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<DialogPopupComponent>, public socialLogin: SocialLoginService) { }
 
-  ngOnInit(): void {
-    // this.socialLogin.fbInit();
-    console.log('got here');
-    (window as any).fbAsyncInit = () => {
-      FB.init({
-        appId      : '635708387067929',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v7.0'
-      });
-
-      FB.AppEvents.logPageView();
-    };
-
-    ((d, s, id) => {
-      // tslint:disable-next-line: one-variable-per-declaration
-      let js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return; }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
-  }
+  ngOnInit(): void { }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
-  fbSubmitLogin() {
-    // this.socialLogin.fbSubmitLogin();
+  fbSubmitLogin(){
+    // FB.getLoginStatus((response: any) => {   // Called after the JS SDK has been initialized.
+    //   this.statusChangeCallback(response);        // Returns the login status.
+    // });
+
     console.log('submit login to facebook');
-    FB.login();
+
+    FB.login((response: any) => {
+      console.log(response);
+      this.statusChangeCallback(response);
+    }, {scope: 'public_profile,email'});
+  }
+
+  statusChangeCallback(response: any) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      this.testAPI();
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      console.log('Log in failed');
+    }
+  }
+
+  testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', (response: any) => {
+      console.log(response);
+    });
+  }
+
+  fbLogout() {
+    FB.logout((response: any) => {
+      console.log('you were logged out');
+   });
   }
 
 }
