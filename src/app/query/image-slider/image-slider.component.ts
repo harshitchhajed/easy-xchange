164733f76556aa6781941c65e32a3ea32d8a8eb5
harshit <h.d.chhajed@gmail.com>
2020-08-03 19:33:38 +0530

    added google static map, removed api key

--- src/app/query/image-slider/image-slider.component.ts
+++ src/app/query/image-slider/image-slider.component.ts
@@ -13,7 +13,13 @@ import { trigger, transition, style, animate } from '@angular/animations';
           opacity: 1,
           transform: 'scale(1)'
         }))
-      ])
+      ]),
+      // transition('* => void', [
+      //   animate('500ms cubic-bezier(0.785, 0.135, 0.15, 0.86)', style({
+      //     opacity: 0,
+      //     transform: 'scale(0.75)'
+      // }))
+      // ])
     ])
   ]
 })
--- src/app/query/items/item-card/item-card.component.ts
+++ src/app/query/items/item-card/item-card.component.ts
@@ -9,6 +9,12 @@ import { ItemsQueryService } from '../../items-query.service';
 export class ItemCardComponent implements OnInit {
   pics: string[];
   itemName: string;
+  staticMapsUrl: string;
+  lat = '49.2614434';
+  long = '-123.2588847';
+  // TODO: remove api key from front-end
+  key = '';
+  mapShown = false;
 
   constructor(private itemsService: ItemsQueryService) { }
 
@@ -19,8 +25,15 @@ export class ItemCardComponent implements OnInit {
       'https://www.ikea.com/in/en/images/products/flintan-nominell-office-chair-with-armrests__0723346_PE733927_S5.JPG?f=g',
       'https://www.ikea.com/in/en/images/products/flintan-nominell-office-chair-with-armrests__0723347_PE733930_S5.JPG?f=g',
       'https://www.ikea.com/in/en/images/products/flintan-nominell-office-chair-with-armrests__0854761_PE563185_S5.JPG?f=g'
-    ],
-    this.itemName = 'Office Chair';
-   }
+    ];
 
+    this.itemName = 'Office Chair';
+
+    this.staticMapsUrl = `https://maps.googleapis.com/maps/api/staticmap?center=University+of+British+Columbia,Vancouver,BC
+      &zoom=13
+      &size=320x400
+      &maptype=roadmap
+      &markers=color:red%7C${this.lat},${this.long}
+      &key=${this.key}`;
+   }
 }
