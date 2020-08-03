386a898cdb25d026aa89eda625d5fc14dddb22b0
harshit <h.d.chhajed@gmail.com>
2020-08-03 17:12:13 +0530

    nice animation for the image-slider

--- src/app/query/image-slider/image-slider.component.ts
+++ src/app/query/image-slider/image-slider.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-image-slider',
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.css'],
+  animations: [
+    trigger('sliderAnimation', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('300ms', style({
+          opacity: 1
+        }))
+      ]),
+      transition('* => void', [
+        animate('0ms', style({
+          opacity: 0
+      }))
+      ])
+    ])
+  ]
 })
 export class ImageSliderComponent implements OnInit {
   @Input() slides: string[];
@@ -18,13 +34,11 @@ export class ImageSliderComponent implements OnInit {
   onPreviousClick() {
     const previous = this.currentSlide - 1;
     this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
-    console.log('previous clicked, new current slide is: ', this.currentSlide);
   }
 
   onNextClick() {
     const next = this.currentSlide + 1;
     this.currentSlide = next === this.slides.length ? 0 : next;
-    console.log('next clicked, new current slide is: ', this.currentSlide);
   }
 
 }
