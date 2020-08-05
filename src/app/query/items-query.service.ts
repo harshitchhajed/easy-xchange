5370a028eb679a7786dcf62dffa952f080e58445
harshit <h.d.chhajed@gmail.com>
2020-08-05 21:55:23 +0530

    client receiving firestore data correctly

--- src/app/query/items-query.service.ts
+++ src/app/query/items-query.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, DocumentData } from '@angular/fire/firestore';
 import { ItemQuery } from './item-query';
 
 @Injectable({
@@ -12,33 +12,10 @@ export class ItemsQueryService {
 
   // TODO: pass in queries like this. In future: server infers from the url
   getItemsFromServer(item: string, date: string) {
-    // maybe some http get request
-    const chair = this.firestore.collection('/items').doc('0NZoYnkYj3zyuSLOmb2d').get().subscribe(snapshot => {
-      this.items.push(snapshot.data() as ItemQuery);
-      return snapshot;
+
+    this.firestore.collection('/items').doc('0NZoYnkYj3zyuSLOmb2d').get().subscribe(snapshot => {
+      this.items.push(snapshot.data()  as ItemQuery);
     });
-
-    console.log(chair);
-
-    // const desk: ItemQuery = {name: 'Desk', photos: [
-    //   'https://www.ikea.com/in/en/images/products/micke-desk__0736019_PE740346_S5.JPG?f=xs',
-    //   'https://www.ikea.com/in/en/images/products/micke-desk__0403166_PE565225_S5.JPG?f=xs',
-    //   'https://www.ikea.com/in/en/images/products/micke-desk__0851577_PE660344_S5.JPG?f=xs',
-    //   'https://www.ikea.com/in/en/images/products/micke-desk__0403462_PE565521_S5.JPG?f=xs'
-    //   ],
-    //   location: {
-    //     'Exchange Residence',
-    //     {
-    //       49.0,
-    //        -123.0,
-    //       }},
-    //   pricePerHour: 10.50
-    // };
-
-    // const tempItems = [desk, desk, desk, desk, desk, desk, desk];
-
-    // this.items = tempItems;
-
   }
 
   getItems() {
