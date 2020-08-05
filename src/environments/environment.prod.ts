d3c7dd2c7e5db346a6ab0ec807cd043b3313f99b
harshit <h.d.chhajed@gmail.com>
2020-08-05 16:52:40 +0530

    added firebase for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDzQbg9aIKeeRnzzm7dTkXXjHdhSHNO068',
+    authDomain: 'easy-xchange.firebaseapp.com',
+    databaseURL: 'https://easy-xchange.firebaseio.com',
+    projectId: 'easy-xchange',
+    storageBucket: 'easy-xchange.appspot.com',
+    messagingSenderId: '172126270901'
+  }
 };
