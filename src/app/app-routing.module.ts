e3570d8a2b95b24c396fb48c70452048f21d5ab0
harshit <h.d.chhajed@gmail.com>
2020-08-15 12:31:05 +0530

    removed unnecessary import comments from routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,21 +6,6 @@ import { ItemsComponent } from './query/items/items.component';
 import { RentItemComponent } from './submit/rent-item/rent-item.component';
 import { DetailedItemComponent } from './query/items/detailed-item/detailed-item.component';
 
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatAutocompleteModule } from '@angular/material/autocomplete';
-// import { MatChipsModule } from '@angular/material/chips';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatDividerModule } from '@angular/material/divider';
-// import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { MatNativeDateModule } from '@angular/material/core';
-// import { MatTabsModule } from '@angular/material/tabs';
-// import { MatGridListModule } from '@angular/material/grid-list';
-// import { MatCardModule } from '@angular/material/card';
-// import { MatToolbarModule } from '@angular/material/toolbar';
-// import { MatDialogModule } from '@angular/material/dialog';
-
-
 const routes: Routes = [
   {path: '', component: FrontPageComponent, pathMatch: 'full'},
   {path: 'signup', component: SignupComponent},
