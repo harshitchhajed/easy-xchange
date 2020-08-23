3c6d3f71863bbfc60cba6a04cfdca173a03afb47
harshit <h.d.chhajed@gmail.com>
2020-08-23 12:18:10 +0530

    can search for any item, redirect to all items

--- src/app/query/items/items.component.ts
+++ src/app/query/items/items.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-items',
@@ -8,37 +8,58 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit, OnDestroy {
-  items: any[] = [];
+  items = [];
   loaded = false;
 
   constructor(private firestore: AngularFirestore,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
 
     // this.items = this.itemsService.getItems();
-    const item = this.route.snapshot.queryParams.item;
-    const date = this.interpretDateFromURL().toLocaleDateString();
+    let item = this.route.snapshot.queryParams.item;
+    let date = this.interpretDateFromURL(this.route.snapshot.queryParams.date).toLocaleDateString();
+    this.route.queryParams.subscribe((params: Params) => {
+      item = params.item;
+      date = params.date;
+      this.fetchItems(item, date);
+    });
 
-    this.firestore.collection('items', (ref) => {
-      return ref.where('name', '==', item);
-    })
-      .get()
-      .subscribe(snapshot => {
-        snapshot.forEach(doc => {
-          this.items.push(doc);
-        });
-        this.loaded = true;
-      });
+    this.fetchItems(item, date);
   }
 
   ngOnDestroy() {
     this.items = [];
   }
 
-  interpretDateFromURL() {
+  fetchItems(item: string, date: string) {
+    if (item === 'all') {
+      this.firestore.collection('items')
+        .get()
+        .subscribe(snapshot => {
+          snapshot.forEach(doc => {
+            this.items.push(doc);
+          });
+          this.loaded = true;
+        });
+    } else {
+      this.firestore.collection('items', (ref) => {
+        return ref.where('name', '==', item);
+      })
+        .get()
+        .subscribe(snapshot => {
+          snapshot.forEach(doc => {
+            this.items.push(doc);
+          });
+          this.loaded = true;
+        });
+    }
+  }
 
-    const dates = this.route.snapshot.queryParams.date.split('-');
+  interpretDateFromURL(url: string) {
+
+    const dates = url.split('-');
     console.log(dates);
     try {
       if (dates.length !== 3) {
@@ -50,5 +71,12 @@ export class ItemsComponent implements OnInit, OnDestroy {
       console.log(error);
       // add 404 routing
     }
+  }
+
+  searchAll() {
+    this.router.navigate(['/items'], {
+      queryParams: {item: 'all'},
+      queryParamsHandling: 'merge'
+    });
   }
 }
